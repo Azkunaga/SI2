f01e5ea3cd2e1bcbaef80374f26d96b3abbd1da7
jauregi13 <jon.j1305@gmail.com>
2021-11-14 15:25:44 +0100

    Main aldatuta Iterator paketean

--- src/main/java/Iterator/Main.java
+++ src/main/java/Iterator/Main.java
@@ -1,34 +1,46 @@
 package Iterator;
 
+import java.net.MalformedURLException;
 import java.util.Date;
 
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
+import configuration.ConfigXML;
 import domain.Event;
+import gui.ApplicationLauncherFactory;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		BLFacade facade = new BLFacadeImplementation();
-		
-		EventIterator<Event> events = (EventIterator)facade.getEvents(new Date("2021/12/17"), "Futbola", "LaLiga");
-		
-		// Bukaeratik
-		events.goLast();
-		while (events.hasPrevious()) {
-			Event event = (Event) events.previous();
-			System.out.println(event.toString());
+		ConfigXML c=ConfigXML.getInstance();
+		ApplicationLauncherFactory apf= new ApplicationLauncherFactory(); 
+		BLFacade appFacadeInterface;
+		try {
+			appFacadeInterface = apf.createAL(c);
+			EventIterator<Event> events = (EventIterator)appFacadeInterface.getEvents(new Date("2021/12/17"), "Futbola", "LaLiga");
 			
+			// Bukaeratik
+			events.goLast();
+			while (events.hasPrevious()) {
+				Event event = (Event) events.previous();
+				System.out.println(event.toString());
+				
+			}
+			
+			// Hasieratik
+			events.goFirst();
+			while (events.hasNext()) {
+				Event event = (Event) events.next();
+				System.out.println(event.toString());
+				
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		// Hasieratik
-		events.goFirst();
-		while (events.hasNext()) {
-			Event event = (Event) events.next();
-			System.out.println(event.toString());
-			
-		}
+		
 		
 	}
 
